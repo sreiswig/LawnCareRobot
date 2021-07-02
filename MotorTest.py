6fab33a182137188e6db1def3907a173a0a6c2fb
sam_reiswig <sam.reiswig@gmail.com>
2021-07-01 21:14:05 -0700

    Adds Motor Class and Motor Controller Class

--- MotorTest.py
+++ MotorTest.py
@@ -1,23 +1,26 @@
 import Jetson.GPIO as GPIO
+from L298NMotorDriveController import MotorController
+from Motor import Motor
 import time
 
 GPIO.setmode(GPIO.BOARD)
 
 # TO-DO: Read from JSON provided from some db somewhere over the rainbow
-
 Channels = (35, 37)
 
 GPIO.setup(Channels, GPIO.OUT)
 
-GPIO.output(35, GPIO.HIGH)
+motor1 = Motor([35, 37])
+
+motor1.RotateClockwise()
 time.sleep(5)
 
-GPIO.output(35, GPIO.LOW)
-time.sleep(1)
-
-GPIO.output(37, GPIO.HIGH)
+motor1.Stop()
 time.sleep(5)
 
-GPIO.output(37, GPIO.LOW)
+motor1.RotateCounterClockwise()
+time.sleep(5)
+
+motor1.Stop()
 
 GPIO.cleanup()
