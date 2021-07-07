e34d09f16b1d2b99f667165c2fe50ebc07a0733a
sam_reiswig <sam.reiswig@gmail.com>
2021-07-06 20:53:42 -0700

    Adds: Placeholder pump and arm controls

--- TerminalInput.py
+++ TerminalInput.py
@@ -9,16 +9,21 @@ import time
 GPIO.setmode(GPIO.BOARD)
 
 # TO-DO: Make and Read from JSON init file
-Channels = [31, 33, 35, 37]
+Channels = [27, 29, 31, 33, 35, 37]
 motor1Pins = [35, 37]
 motor2Pins = [31, 33]
+armPins = [27, 29]
+pumpPins = [23, 25]
 
 GPIO.setup(Channels, GPIO.OUT)
 
 motor1 = Motor(motor1Pins)
 motor2 = Motor(motor2Pins)
+arm = Motor(armPins)
+pump = Motor(pumpPins)
 
 DriveController = L298NTrackMotorController(motor1, motor2)
+ArmController = L298NArmAndPumpController(pump, arm)
 
 command = "start"
 while command != "stop":
@@ -39,6 +44,11 @@ while command != "stop":
     if(command == " "):
         DriveController.Stop()
         continue
+    if(command == "q"):
+        ArmController.RotateArmLeft()
+        continue
+    if(command == "e"):
+        ArmController.RotateArmRight()
 
 DriveController.Stop()
 GPIO.cleanup()
