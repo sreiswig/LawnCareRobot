19e81b38b4d6f609c5945a847e95a30fe29ea2c1
sam_reiswig <sam.reiswig@gmail.com>
2021-07-12 21:03:39 -0700

    Add: change of file location for config.json

--- Python/MqttTest.py
+++ Python/MqttTest.py
@@ -10,7 +10,7 @@ def on_message(client, userdata, message):
     return message
 
 def read_config():
-    with open("Python/config.json") as config_file:
+    with open("config.json") as config_file:
         config = json.load(config_file)
     
     return config
