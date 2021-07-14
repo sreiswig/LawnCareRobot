928ac736330dcb09336bf66a444cf8ab1e69fe39
sam_reiswig <sam.reiswig@gmail.com>
2021-07-14 14:47:37 -0700

    pins are ints not strings

--- Python/MqttTest.py
+++ Python/MqttTest.py
@@ -61,8 +61,8 @@ config = read_config()
 if "driveController" in config.keys():
     if "motor" in config["driveController"].keys():
         print("Initializing Drive Controller")
-        motor1Pins = [config["driveController"]["motor"][0]["pin1"], config["driveController"]["motor"][0]["pin2"]]
-        motor2Pins = [config["driveController"]["motor"][1]["pin1"], config["driveController"]["motor"][1]["pin2"]]
+        motor1Pins = [int(config["driveController"]["motor"][0]["pin1"]), int(config["driveController"]["motor"][0]["pin2"])]
+        motor2Pins = [int(config["driveController"]["motor"][1]["pin1"]), int(config["driveController"]["motor"][1]["pin2"])]
         driveMotor1 = Motor(motor1Pins)
         driveMotor2 = Motor(motor2Pins)
         DriveController = L298NTrackMotorController(driveMotor1, driveMotor2)
@@ -72,8 +72,8 @@ if "driveController" in config.keys():
 if "pumpAndArmController" in config.keys():
     if "pump" in config["pumpAndArmController"].keys():
         print("Initializing Pump and Arm Controller")
-        pumpPins = [config["pumpAndArmController"]["pump"]["pin1"], config["pumpAndArmController"]["pump"]["pin2"]]
-        armPins = [config["pumpAndArmController"]["arm"]["pin1"], config["pumpAndArmController"]["arm"]["pin2"]]
+        pumpPins = [int(config["pumpAndArmController"]["pump"]["pin1"]), int(config["pumpAndArmController"]["pump"]["pin2"])]
+        armPins = [int(config["pumpAndArmController"]["arm"]["pin1"]), int(config["pumpAndArmController"]["arm"]["pin2"])]
         pump = Motor(pumpPins)
         arm = Motor(armPins)
         ArmController = L298NArmAndPumpController(pump, arm)
