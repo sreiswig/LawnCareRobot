18e59b6af6b8b83080d75f9024b5d7ee5c3f7c64
sam_reiswig <sam.reiswig@gmail.com>
2021-07-14 14:03:13 -0700

    Test: Ordering

--- Python/MqttTest.py
+++ Python/MqttTest.py
@@ -5,33 +5,6 @@ from Motor import Motor
 import json
 import paho.mqtt.client as mqtt
 
-
-# Read from some generic config file in json format
-config = read_config()
-if "driveController" in config.keys():
-    if "motor" in config["driveController"].keys():
-        print("driveController initialized")
-        driveMotor1 = Motor([config["driveController"]["motor"][0]["pin1"], config["driveController"]["motor"][0]["pin2"]])
-        driveMotor2 = Motor([config["driveController"]["motor"][1]["pin1"], config["driveController"]["motor"][1]["pin2"]])
-        DriveController = L298NTrackMotorController(driveMotor1, driveMotor2)
-
-if "pumpAndArmController" in config.keys():
-    if "pump" in config["pumpAndArmController"].keys():
-        pump = Motor([config["pumpAndArmController"]["pump"]["pin1"], config["pumpAndArmController"]["pump"]["pin2"]])
-        arm = Motor([config["pumpAndArmController"]["arm"]["pin1"], config["pumpAndArmController"]["arm"]["pin2"]])
-        ArmController = L298NArmAndPumpController(pump, arm)
-
-if "mqtt" in config.keys():
-    mqttClientId = config["mqtt"]["clientId"]
-    mqttHost = config["mqtt"]["host"]
-    client = mqtt.Client(mqttClientId)
-    client.connect(mqttHost)
-    # attach to callback
-    client.on_message = on_message
-    client.loop_start()
-    client.publish("robotCommands", "Robot Initialized")
-    client.subscribe("robotCommands")
-
 def on_message(client, userdata, message):
     print("message received " , str(message.payload.decode("utf-8")))
     if(message == "w"):
@@ -73,6 +46,32 @@ def read_config():
         config = json.load(config_file)
     
     return config
+
+# Read from some generic config file in json format
+config = read_config()
+if "driveController" in config.keys():
+    if "motor" in config["driveController"].keys():
+        print("driveController initialized")
+        driveMotor1 = Motor([config["driveController"]["motor"][0]["pin1"], config["driveController"]["motor"][0]["pin2"]])
+        driveMotor2 = Motor([config["driveController"]["motor"][1]["pin1"], config["driveController"]["motor"][1]["pin2"]])
+        DriveController = L298NTrackMotorController(driveMotor1, driveMotor2)
+
+if "pumpAndArmController" in config.keys():
+    if "pump" in config["pumpAndArmController"].keys():
+        pump = Motor([config["pumpAndArmController"]["pump"]["pin1"], config["pumpAndArmController"]["pump"]["pin2"]])
+        arm = Motor([config["pumpAndArmController"]["arm"]["pin1"], config["pumpAndArmController"]["arm"]["pin2"]])
+        ArmController = L298NArmAndPumpController(pump, arm)
+
+if "mqtt" in config.keys():
+    mqttClientId = config["mqtt"]["clientId"]
+    mqttHost = config["mqtt"]["host"]
+    client = mqtt.Client(mqttClientId)
+    client.connect(mqttHost)
+    # attach to callback
+    client.on_message = on_message
+    client.loop_start()
+    client.publish("robotCommands", "Robot Initialized")
+    client.subscribe("robotCommands")
 
 # Terminate using command line
 def main():    
