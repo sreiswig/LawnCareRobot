e448129fdb8d09b0452cefa0693859b72cf1456b
sam_reiswig <sam.reiswig@gmail.com>
2021-07-13 20:39:43 -0700

    Adds: read initialization pins and mqtt information from config.json

--- Python/MqttTest.py
+++ Python/MqttTest.py
@@ -19,16 +19,29 @@ def read_config():
 def main():
     # Read from some generic config file in json format
     config = read_config()
-    mqttConfig = config["mqtt"]
-    client = mqtt.Client("LawnRobot")
-    client.connect(mqttConfig["host"])
+    if "driveController" in config.keys():
+        if "motor" in config["driveController"].keys():
+            driveMotor1 = Motor([config["driveController"]["motor"][0]["pin1"], config["driveController"]["motor"][0]["pin2"]])
+            driveMotor2 = Motor([config["driveController"]["motor"][1]["pin1"], config["driveController"]["motor"][1]["pin2"]])
+            driveController = L298NTrackMotorController(driveMotor1, driveMotor2)
 
-    client.publish("robotCommands", "Robot Initialized")
+    if "pumpAndArmController" in config.keys():
+        if "pump" in config["pumpAndArmController"].keys():
+            pump = Motor([config["pumpAndArmController"]["pump"]["pin1"], config["pumpAndArmController"]["pump"]["pin2"]])
+            arm = Motor([config["pumpAndArmController"]["arm"]["pin1"], config["pumpAndArmController"]["arm"]["pin2"]])
+            pumpAndArmController = L298NArmAndPumpController(pump, arm)
 
-    # attach to callback
-    client.on_message = on_message
-    client.loop_start()
-    client.subscribe("robotCommands")
+    if "mqtt" in config.keys():
+        mqttClientId = config["mqtt"]["clientId"]
+        mqttHost = config["mqtt"]["host"]
+        client = mqtt.Client(mqttClientId)
+        client.connect(mqttHost)
+        # attach to callback
+        client.on_message = on_message
+        client.loop_start()
+        client.subscribe("robotCommands")
+        client.publish("robotCommands", "Robot Initialized")
+        
     command = "start"
     while command != "stop":
         command = input("Enter 'stop' to quit: ")
