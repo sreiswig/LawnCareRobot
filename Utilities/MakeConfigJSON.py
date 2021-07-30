c50c74161cd17440ef5d21c661010ed39337b297
sam_reiswig <sam.reiswig@gmail.com>
2021-07-30 09:08:40 -0700

    Minor Housekeeping

---
+++ Utilities/MakeConfigJSON.py
@@ -0,0 +1,30 @@
+import json
+
+# Hardcode the Questions for config for now.
+config = []
+Motor1 = {}
+Motor2 = {}
+Pump = {}
+Arm = {}
+DriveController = {}
+PumpAndArmController = {}
+MqttCredentials = {}
+
+Motor1["pin1"] = input("Enter Motor1 pin1: ")
+Motor1["pin2"] = input("Enter Motor1 pin2: ")
+Motor2["pin1"] = input("Enter Motor2 pin1: ")
+Motor2["pin2"] = input("Enter Motor2 pin2: ")
+Pump["pin1"] = input("Enter Pump pin1: ")
+Pump["pin2"] = input("Enter Pump pin2: ")
+Arm["pin1"] = input("Enter Arm pin1: ")
+Arm["pin2"] = input("Enter Arm pin2: ")
+Arm["pwm"] = input("Enter PWM pin: ")
+DriveController = {"motor" : [ Motor1 , Motor2 ]}
+PumpAndArmController = { "pump" : Pump , "arm" : Arm }
+MqttCredentials["clientId"] = input("Enter MQTT clientId: ")
+MqttCredentials["host"] = input("Enter Host IP: ")
+
+config = { "driveController" : DriveController, "pumpAndArmController" : PumpAndArmController, "mqtt" : MqttCredentials }
+
+with open('config.json', 'w') as outfile:
+    json.dump(config, outfile)
